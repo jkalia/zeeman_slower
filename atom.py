2723c66bf512231fbe96ff8e9fa0d547e47378c3
Jasmine Kalia <jasminekalia820@gmail.com>
2021-07-09 10:14:38 -0400

    added an Atom class

---
+++ atom.py
@@ -0,0 +1,25 @@
+# Jasmine Kalia
+# July 9th, 2021
+# atom.py
+# Creates an Atom class which stores the variables for an erbium atom or for a 
+# lithium atom 
+
+
+import ideal_field as ideal
+
+
+class Atom:
+    def __init__(self, atom_type):
+        if atom_type == "Er":
+            self.m = ideal.m_er
+            self.linewidth = ideal.linewidth_er
+            self.k = ideal.k_er
+            self.mu0 = ideal.mu0_er
+            self.Isat = ideal.Isat_er
+        elif atom_type == "Li":
+            self.m = ideal.m_li
+            self.linewidth = ideal.linewidth_li
+            self.k = ideal.k_li
+            self.mu0 = ideal.mu0_li
+            self.Isat = ideal.Isat_li_d2
+
